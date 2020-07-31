2cba43a5c8ee98d9d1957c0e3ba2958c18b0a870
Leonardo Seiji Teshima <teshimaleo@gmail.com>
2020-07-31 12:56:44 -0300

    Atualizando

--- src/pages/Cadastro/Categoria/index.js
+++ src/pages/Cadastro/Categoria/index.js
@@ -4,13 +4,27 @@ import { Link } from 'react-router-dom'
 
 function CadastroCategoria() {
     return (
-            <PageDefault>
-                <h1>Cadastro de Categoria</h1>
-                
-                <Link to='/'>
-                    Ir pra Home
+        <PageDefault>
+            <h1>Cadastro de Categoria</h1>
+            <form>
+
+                <label>
+                    Nome da Categoria:
+          <input
+                        type="text"
+                    />
+                </label>
+
+                <button>
+                    Cadastrar
+        </button>
+            </form>
+
+
+            <Link to='/'>
+                Ir pra Home
             </Link>
-            </PageDefault>
+        </PageDefault>
     )
 }
 
